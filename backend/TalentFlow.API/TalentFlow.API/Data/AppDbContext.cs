4328c062967b53eb57b46c1e46b5f7a8199cf677
Nimsara Lekamge <nimsaralekamge@gmail.com>
2026-07-20 04:01:56 +0530

    backend structure

---
+++ backend/TalentFlow.API/TalentFlow.API/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TalentFlow.API.Models; // This MUST match the folder name "Models"
+
+namespace TalentFlow.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Interview> Interviews { get; set; }
+        public DbSet<Evaluation> Evaluations { get; set; }
+    }
+}
---
+++ backend/TalentFlow.API/TalentFlow.API/Models/Evaluation.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations; // You MUST have this line!
+
+namespace TalentFlow.API.Models
+{
+    public class Evaluation
+    {
+        [Key]
+        public int Id { get; set; }
+        // ... rest of your code
+    }
+}
