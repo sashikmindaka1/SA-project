4ae1d3ffd7c7574203dd4b7232f41fbe4cfb99a0
sashik mindaka <mindakaofficial0069@gmail.com>
2026-07-23 19:57:46 +0530

    update profie candidate

---
+++ backend/TalentFlow.API/TalentFlow.API/Migrations/20260723142527_AddPhotoAndResumeColumns.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TalentFlow.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPhotoAndResumeColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PhotoFileName",
+                table: "CandidateProfiles",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhotoUrl",
+                table: "CandidateProfiles",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhotoFileName",
+                table: "CandidateProfiles");
+
+            migrationBuilder.DropColumn(
+                name: "PhotoUrl",
+                table: "CandidateProfiles");
+        }
+    }
+}
